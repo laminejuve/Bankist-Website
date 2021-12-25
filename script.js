4c9628b59663e3425054faa3786df6710e4fc282
lamine <laminejuvee@gmail.com>
2021-12-25 23:22:26 +0100

    built a tabbed component

--- script.js
+++ script.js
@@ -62,5 +62,24 @@ document.querySelector(".nav__links").addEventListener('click',function(e){
   }
 });
 
+// tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+console.log(tabsContainer);
 
+tabsContainer.addEventListener('click',function(e){
+  const clicked = e.target.closest('.operations__tab');
+  console.log(clicked);
+  // guard clause 
+  if(!clicked) return ;
+  //remove active classes
+  tabs.forEach(tab => tab.classList.remove('operations__tab--active'));
+  tabsContent.forEach(tab => tab.classList.remove('operations__content--active'));
+ //active tab
+  clicked.classList.add('operations__tab--active');
+  //active content area
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList
+                         .add('operations__content--active');
+});
 
