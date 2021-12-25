9cfc17b448f32d6e433079745f56a4ebbd5dab24
lamine <laminejuvee@gmail.com>
2021-12-25 14:09:35 +0100

    impement page navigation smooth mode

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+
+///////////////////////////////////////
+// Modal window
 
 const openModal = function (e) {
   e.preventDefault();
@@ -43,10 +45,22 @@ document.querySelector(".btn--close-cookie").addEventListener('click',function()
 
 // implementing smooth scrolling
 
-const btnScrollTo = document.querySelector(".btn--scroll-to");
-const section1 = document.querySelector("#section--1");
 btnScrollTo.addEventListener('click',function(){
   section1.scrollIntoView({behavior : 'smooth'});
 })
+
+// page navigation
+   // 1- add event listener to common parent element
+   // 2- determine what element originated the event
+
+document.querySelector(".nav__links").addEventListener('click',function(e){
+  e.preventDefault();
+  // matching strategy
+  if (e.target.classList.contains("nav__link")){
+     const id = e.target.getAttribute("href");
+     document.querySelector(id).scrollIntoView({behavior: 'smooth'});
+  }
+});
+
 
 
