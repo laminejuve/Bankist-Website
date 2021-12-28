a97dec925af2c7533924b550e2dc93d62562848f
lamine <laminejuvee@gmail.com>
2021-12-28 11:48:30 +0100

    laoding lazy images

--- script.js
+++ script.js
@@ -138,8 +138,27 @@ const revealSection = function (entries , observer){
 const sectionObserver = new IntersectionObserver(revealSection , {
   root : null,
   threshold : 0.15 ,
+  rootMargin : '200px'
 });
 allSections.forEach(section => {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
-})
+});
+
+// lazy loading images 
+
+const imgTargets = document.querySelectorAll('img[data-src]');
+const loadImg = function (entries , observer){
+  const [entry] = entries ;
+  if (!entry.isIntersecting) return ;
+  entry.target.src = entry.target.dataset.src ;
+  entry.target.addEventListener('load',function(){
+    entry.target.classList.remove('lazy-img');
+  });
+  observer.unobserve(entry.target);
+  
+}
+const imgObserver = new IntersectionObserver(loadImg , {
+
+});
+imgTargets.forEach(img => imgObserver.observe(img));
